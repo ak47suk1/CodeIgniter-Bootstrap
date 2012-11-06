5f3dd713376d515f3e60423480ee020a482b3919
Steven Lu <tacticalazn@gmail.com>
2012-11-05 23:35:25 -0500

    Comments.

--- installer/application/controllers/installer.php
+++ installer/application/controllers/installer.php
@@ -2,10 +2,25 @@
 
 class Installer extends CI_Controller {
 
+   /**
+    * Default variables.
+    * Order does matter here
+    * cause we will loop through
+    * this in order to install things
+    * properly.
+    */
    private $args = array(
+      '--with-codeigniter' => true,
+      '--with-twitter-boostrap' => true,
+      '--with-font-awesome' => true,
       '--with-grunt' => false
    );
 
+   /**
+    * Default instance call
+    * This will print to the command line
+    * what the available options are
+    */
    function index()
    {
 
@@ -24,12 +39,18 @@ Usage: php index.php build <args>
 \n
 ";
 
-
    }
 
+   /**
+    * Build function
+    * We will execute the build here.
+    */
 	function build()
 	{
 
+      // grab all the command line arguments
+      // and parse them appropriately into 
+      // in the defaults variable of this class
       $n = 1;
       while (($arg = $this->uri->segment($n)) !== FALSE)
       {
@@ -43,11 +64,14 @@ Usage: php index.php build <args>
 
       }
 
+      // load the required models.
       $this->load->model(array(
          "twitter_bootstrap_model",
          "code_igniter_model",
          "font_awesome_model"
       ));
+
+      
 
 
    }
